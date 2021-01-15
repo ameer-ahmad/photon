844501657b58412665cdd5bc0cca857241534ac1
ammer-ahmad <ameer5ahmad@gmail.com>
2021-01-15 09:03:10 -0500

    added search functionality

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const auth = "563492ad6f91700001000001e6a1f00b126646ee899fc85bc5d7e705";
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
-const submitButton = document.querySelector('.submit-btn');
+const form = document.querySelector('.search-form');
 let searchValue;
+
+
+//Event Listeners
+searchInput.addEventListener('input', updateInput);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+    searchValue = e.target.value;
+}
 
 async function curratedPhotos() {
     const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1", {
@@ -13,7 +25,25 @@ async function curratedPhotos() {
         }
     });
     const data = await dataFetch.json();
-    console.log(data);
+    data.photos.forEach(photo => {
+        const galleryImg = document.createElement('div');
+        galleryImg.classList.add('gallery-img');
+        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+        <p>${photo.photographer}</p>
+        `;
+        gallery.appendChild(galleryImg);
+    });
+}
+
+async function searchPhotos(query) {
+    const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            Authorization: auth
+        }
+    });
+    const data = await dataFetch.json();
     data.photos.forEach(photo => {
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
